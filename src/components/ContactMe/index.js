af1a6cea47dd5eb6b707f11be5e7798ef3a8dbd0
Michael <fraynemichael300@gmail.com>
2023-04-05 01:15:31 -0600

    stuck with useState err

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
-function ContactForm() {
+let grecaptcha;
+
+async function ContactForm () {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -49,15 +51,29 @@ function ContactForm() {
     });
   };
 
-  const handleFormSubmit = async (event) => {
+  function validate(event) {
+    event.preventDefault();
+    if (!document.getElementById('field').value) {
+      alert("You must add text to the required field");
+    } else {
+      grecaptcha.execute();
+    }
+  }
+
+  const onload = () => {
+    var element = document.getElementById('submit');
+    element.onclick = validate;
+  }
+  const handleFormSubmit = async (token, event) => {
+    onload();
     event.preventDefault();
     if (formIsValid) {
       try {
         const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
         const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
         const userId = process.env.REACT_APP_EMAILJS_USER_ID;
-        
-        await emailjs.send(serviceId, templateId, {
+        emailjs.init(process.env.REACT_APP_EMAILJS_USER_ID);
+        emailjs.send(serviceId, templateId, {
           from_name: formData.name,
           from_email: formData.email,
           message: formData.message
@@ -80,7 +96,8 @@ function ContactForm() {
         console.log('Error occurred while sending email: ', error);
       }
     }
-  };  
+  };
+  
 
   const inputFields = [
     { name: "name", type: "text", label: "Name" },
@@ -88,33 +105,22 @@ function ContactForm() {
     { name: "message", type: "textarea", label: "Message" },
   ];
 
-  return (
-    <div className="flex flex-col justify-center items-center z-0">
-      <h2 className="text-4xl lg:text-5xl font-bold mb-5 self-center lg:self-start lg:ml-10">Contact Me</h2>
-      <form className="flex flex-col self-center lg:self-start lg:ml-10 lg:w-1/2 md:w-7/12" onSubmit={handleFormSubmit}>
-        {inputFields.map(({ name, type, label }) => (
-          <React.Fragment key={name}>
-            <label htmlFor={name}>{label}</label>
-            <input
-              type={type}
-              name={name}
-              value={formData[name]}
-              onChange={handleInputChange}
-              className={`border-bk border-2 rounded-md p-2 mb-5 mt-2 bg-transparent ${
-                formErrors[name] && "border-red-500"
-              }`}
-              autoComplete="off"
-              required={name === "name" || name === "email" || name === "message"}
-            />
-            {formErrors[name] && <span className="text-red-500 text-sm mb-5 ml-2">{formErrors[name]}</span>}
-          </React.Fragment>
-        ))}
-        <button type="submit" className="border-bk border-2 rounded-md p-2 w-fit self-center lg:self-start" disabled={!formIsValid}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+return (
+  <div className="flex flex-col justify-center items-center z-0">
+    <h2 className="text-4xl lg:text-5xl font-bold mb-5 self-center lg:self-start lg:ml-10">Contact Me</h2>
+    <form className="flex flex-col self-center lg:self-start lg:ml-10 lg:w-1/2 md:w-7/12" onSubmit={handleFormSubmit} data-size="invisible">
+      {inputFields.map(({ name, type, label }) => (
+        <React.Fragment key={name}>
+          <label htmlFor={name}>{label}</label>
+          <input type={type} name={name} value={formData[name]} onChange={handleInputChange} className={`border-bk border-2 rounded-md p-2 mb-5 mt-2 bg-transparent ${formErrors[name] && "border-red-500"}`} autoComplete="off" required={name === "name" || name === "email" || name === "message"} />
+          {formErrors[name] && <span className="text-red-500 text-sm mb-5 ml-2">{formErrors[name]}</span>}
+        </React.Fragment>
+      ))}
+      <button id="submit" type="submit" className="border-bk border-2 rounded-md p-2 w-fit self-center lg:self-start g-recaptcha" disabled={!formIsValid} data-sitekey='6LfNjV0lAAAAAPlop3cJ_83sAL0IrvXgDwPx9M4G' data-callback={handleFormSubmit}>Submit</button>
+    </form>
+  </div>
+);
+
 }
 
 export default ContactForm;
