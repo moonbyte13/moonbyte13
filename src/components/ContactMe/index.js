9d369cbe06a29f430e01560b302923eab5250c62
Michael <fraynemichael300@gmail.com>
2023-04-05 15:54:22 -0600

    added modal to alert user

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
+import { Dialog, Transition } from '@headlessui/react';
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid';
 
 function ContactForm () {
+
+  // const successIcon = <CheckCircleIcon className="h-8 w-8 text-green-500" />;
+  // const errorIcon = <XCircleIcon className="h-8 w-8 text-red-500" />;
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -15,6 +21,18 @@ function ContactForm () {
   });
 
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalMessage, setModalMessage] = useState('');
+
+
+  useEffect(() => {
+    const errors = Object.values(formErrors);
+    const isValid = errors.every((error) => error === '');
+    setFormIsValid(isValid);
+  }, [formErrors]);
+  
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -51,7 +69,7 @@ function ContactForm () {
       try {
         // Send email
         const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-        const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+        const templateId = 'template_p9v543f';
         const userId = process.env.REACT_APP_EMAILJS_USER_ID;
         emailjs.init(userId);
         await emailjs.send(serviceId, templateId, {
@@ -59,9 +77,9 @@ function ContactForm () {
           from_email: formData.email,
           message: formData.message
         });
-    
+  
         console.log('Email sent successfully.');
-    
+  
         setFormData({
           name: '',
           email: '',
@@ -73,11 +91,30 @@ function ContactForm () {
           message: '',
         });
         setFormIsValid(false);
+        openModal('Email sent', 'Your email has been sent successfully.');
+        
+        return true; // fix
       } catch (error) {
         console.log('Error occurred while sending email: ', error);
+        openModal('Error', 'An error occurred while sending your email. Please try again later.');
       }
     }
+    
+    return false; // fix
   };
+  
+  
+  function openModal(title, message) {
+    setModalTitle(title);
+    setModalMessage(message);
+    setIsModalOpen(true);
+  }
+  
+  function closeModal() {
+    setModalTitle('');
+    setModalMessage('');
+    setIsModalOpen(false);
+  }
   
 
   const inputFields = [
@@ -92,11 +129,14 @@ function ContactForm () {
         Contact Me
       </h2>
       <form
+        action="?"
+        method="POST"
         className="flex flex-col self-center lg:self-start lg:ml-10 lg:w-1/2 md:w-7/12"
         onSubmit={handleFormSubmit}
+        data-size="invisible"
       >
         {inputFields.map(({ name, type, label }) => (
-          <div key={name}>
+          <React.Fragment key={name}>
             <label htmlFor={name}>{label}</label>
             <input
               type={type}
@@ -114,7 +154,7 @@ function ContactForm () {
                 {formErrors[name]}
               </span>
             )}
-          </div>
+          </React.Fragment>
         ))}
         <button
           id="submit"
@@ -125,9 +165,39 @@ function ContactForm () {
           Submit
         </button>
       </form>
-    </div>
-  );
+      {isModalOpen && (
+        <Transition.Root show={isModalOpen} as={React.Fragment}>
+          <Dialog
+            onClose={closeModal}
+            className="fixed z-10 inset-0 overflow-y-auto"
+          >
+            <div className="flex items-center justify-center min-h-screen">
+              <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
   
+              <div className="modal-box bg-white rounded-md shadow-xl p-8 w-11/12 lg:w-1/2">
+                <div className="flex justify-end">
+                  <button onClick={closeModal}>
+                    <XCircleIcon className="h-6 w-6 text-gray-600 hover:text-gray-800" />
+                  </button>
+                </div>
+                <div className="text-center">
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    <CheckCircleIcon className="h-6 w-6 text-green-600" />
+                  </div>
+                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 mt-4">
+                    {modalTitle}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">{modalMessage}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+      )}
+    </div>
+  );  
 }
 
 export default ContactForm;
