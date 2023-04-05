7a8023a6a5b0d2e392e40918e61402b3cdc7682b
Michael <fraynemichael300@gmail.com>
2023-04-05 16:58:47 -0600

    added project card mobile functionality

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -59,27 +59,43 @@ function Projects () {
   return (
     <div className="flex flex-col justify-center items-center z-0">
       <h2 className="text-4xl lg:text-5xl font-bold mb-5 self-center lg:self-start lg:ml-10">Projects</h2>
-      <div className="flex flex-row flex-wrap justify-center items-center">
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 place-items-center w-screen">
         {projects.map((project) => (
-          <div key={project.id} className="projCard relative m-4 w-1/3 text-white group">
-            <div className='opacity-100 group-hover:opacity-25 transition duration-500 ease-in-out'>
-              <img src={project.image} alt={project.name} className='imgCard border-transparent rounded-lg h-80'/>
-            </div>
-            <div className="projectText absolute top-0 left-0 px-16 z-10 w-full h-full flex flex-col justify-center items-center group hover:opacity-100 transition duration-500 ease-in-out">
-              <div className='w-full flex flex-row justify-start'>
-                <a href={project.deployedLink} className='col-start-2 col-span-3 self-center opacity-0 group-hover:opacity-25 hover:!opacity-100 transition duration-500 ease-in-out'target='_blank' rel='noopener noreferrer'>
-                  <h3 className="group-hover/name:opacity-full text-3xl font-bold transition duration-500 ease-in-out">{project.name}</h3>
-                </a>
-                <a href={project.githubLink} className='col-start-5 pl-5 opacity-0 group-hover:opacity-25 hover:!opacity-100 transition duration-500 ease-in-out' target='_blank' rel='noopener noreferrer'>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="24px"
-                    width="48px"
-                    height="48px"
-                    viewBox="0,0,256,256"
-                    style={style}
-                  >
+          <div key={project.id} className="projCard relative m-4 w-fit text-white group">
+          <div className="opacity-25 lg:opacity-100 group-hover:opacity-25 transition duration-500 ease-in-out">
+            <img
+              src={project.image}
+              alt={project.name}
+              className="imgCard border-transparent rounded-lg h-80 object-cover w-full"
+            />
+          </div>
+          <div className="projectText absolute top-0 left-0 px-16 z-10 w-full h-full flex flex-col justify-center items-center group">
+            <div className="w-full flex flex-row justify-start">
+              <a
+                href={project.deployedLink}
+                className="col-start-2 col-span-3 self-center group-hover:opacity-25 focus:opacity-25 lg:opacity-0 hover:opacity-100 transition duration-500 ease-in-out"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h3 className="group-hover/name:opacity-full text-3xl font-bold transition duration-500 ease-in-out">
+                  {project.name}
+                </h3>
+              </a>
+              <a
+                href={project.githubLink}
+                className="col-start-5 pl-5 lg:opacity-0 opacity-100 group-hover:opacity-25 focus:opacity-25 hover:opacity-100 transition duration-500 ease-in-out"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="24px"
+                  width="48px"
+                  height="48px"
+                  viewBox="0,0,256,256"
+                  style={style}
+                >
                     <g
                       fill="#fff"
                       fill-rule="nonzero"
@@ -103,7 +119,7 @@ function Projects () {
                   </svg>
                 </a>
               </div>
-              <p className="w-full row-start-2 col-start-2 col-span-5 flex justify-start group-hover:opacity-100 opacity-0 transition-opacity duration-500 ease-in-out">{project.description}</p>
+              <p className="w-full row-start-2 col-start-2 col-span-5 flex justify-start group-hover:opacity-100 lg:opacity-0 opacity-100 transition-opacity duration-500 ease-in-out">{project.description}</p>
             </div>
           </div>        
         ))}
