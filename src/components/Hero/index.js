f28032ff496eb18fb22e9fc2a0f76d845bb41fdc
Michael <fraynemichael300@gmail.com>
2023-04-13 00:32:15 -0600

    added contactme spinner

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,43 +1,37 @@
 import React from 'react';
 import { ReactComponent as DD } from './dd.svg';
-import { ReactComponent as Scroll } from './scroll.svg';
 import './style.css'
 
 function Hero() {
   return (
     <section className="hero h-[100vh] flex-col justify-center mb-24">
       <div
-        className='translate-y-[25vh]'
+        className='translate-y-[20vh]'
       >
         <h2 className='title text-[4vw] leading-[5vw] text-[#89c4a7]'>Michael Frayne is...</h2>
           <div
             className='flex justify-center'
           >
             <img 
-              src="https://readme-typing-svg.demolab.com?font=Nunito&weight=690&size=96&duration=3000&repeat=false&color=B39DDB&center=true&vCenter=true&width=500&height=100&lines=MoonByte" 
+              src="https://readme-typing-svg.demolab.com?font=Space+Mono&weight=700&size=96&duration=3000&pause=1000&color=B39DDB&center=true&vCenter=true&repeat=false&width=500&height=150&lines=MoonByte" 
               alt="Typing SVG" 
-              className='h-[13vw]'
+              className='h-[11vw] scale-x-125'
             />
           </div>
           <div
-            className='flex justify-center mb-6'
+            className='flex justify-center mb-12'
           >
             <img 
               src="https://readme-typing-svg.demolab.com?font=Nunito&weight=690&size=48&duration=1000&pause=2000&color=89C4A7&center=true&vCenter=true&width=300&lines=Developer,;Debugger,;+Designer," 
               alt="Typing SVG" 
-              className='h-[6vw]'
+              className='h-[3vw]'
             />
           </div>
         <div
-          className='flex justify-center animate-bounce'
-        >
-          <Scroll />
-        </div>
-        <div
-          className='flex justify-center animate-bounce'
+          className='flex justify-center animate-[bounce_1s_infinite]'
         >
           <DD 
-            className='w-[3vw]'
+            className='w-[3vw] hover:cursor-pointer'
           />
         </div>
 
