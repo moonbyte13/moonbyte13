d692ac9257c0c269b4d795bd9541a97a3b0f6ad5
Michael <fraynemichael300@gmail.com>
2023-04-15 13:32:01 -0600

    added logos

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -13,14 +13,14 @@ function Hero() {
       <div
         className='translate-y-[20vh] flex flex-col justify-center row-span-7'
       >
-        <h2 className='title text-[4vw] leading-[5vw] text-[#89c4a7]'>Michael Frayne is...</h2>
+        <h2 className='title text-[2rem] md:text-[3rem] lg:text-[4rem] leading-[5vw] text-[#89c4a7]'>Michael Frayne is...</h2>
           <div
             className='flex justify-center'
           >
             <img 
               src="https://readme-typing-svg.demolab.com?font=Space+Mono&weight=700&size=96&duration=3000&pause=1000&color=B39DDB&center=true&vCenter=true&repeat=false&width=500&height=150&lines=MoonByte" 
               alt="Typing SVG" 
-              className='h-[11vw] scale-x-125'
+              className='h-[6em] md:h-[8em] lg:h-[13em] scale-x-125'
             />
           </div>
           <div
@@ -29,7 +29,7 @@ function Hero() {
             <img 
               src="https://readme-typing-svg.demolab.com?font=Nunito&weight=690&size=48&duration=1000&pause=2000&color=89C4A7&center=true&vCenter=true&width=300&lines=Developer,;Debugger,;+Designer," 
               alt="Typing SVG" 
-              className='h-[3vw]'
+              className='h-[2rem] md:h-[3rem] lg:h-[4rem]'
             />
           </div>
       </div>
