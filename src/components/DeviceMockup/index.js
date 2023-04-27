57c741886fc31f7e155ffb1fc2ad3576083b8a92
Michael <fraynemichael300@gmail.com>
2023-04-27 15:29:40 -0600

    added opacity scroll feature for mockups

--- src/components/DeviceMockup/index.js
+++ src/components/DeviceMockup/index.js
@@ -13,20 +13,40 @@ const DeviceMockup = ({ name, deployedLink, description, githubLink }) => {
           observer.unobserve(entry.target);
         } else {
           entry.target.classList.remove('visible');
+          entry.target.style.opacity = ""; // Reset opacity to default when not visible
         }
       });
+
+      const visibleMockups = document.querySelectorAll('.device-mockup.visible');
+
+      visibleMockups.forEach(mockup => {
+        const mockupRect = mockup.getBoundingClientRect();
+        const mockupPosition = mockupRect.top + mockupRect.height / 2;
+        const containerCenter = window.innerHeight / 2;
+        const verticalOffset = Math.abs(mockupPosition - containerCenter) / (window.innerHeight / 2);
+
+        if (verticalOffset < 0.425) {
+          mockup.style.opacity = "1"; // Set full opacity for 75% of the viewport height
+        } else {
+          mockup.style.opacity = `${Math.max(0.25, 1 - (verticalOffset - 0.425) * 2)}`; // Set minimum opacity to 0.25
+        }        
+      });
+
     };
 
     const observerOptions = {
-      threshold: [0.5] // Adjust the threshold value based on your requirements
+      threshold: [0, 0.5, 1] // Add additional thresholds for different opacity values
     };
 
     const observer = new IntersectionObserver(handleIntersection, observerOptions);
     observer.observe(deviceMockupRef.current);
 
+    window.addEventListener('scroll', () => handleIntersection(observer.takeRecords(), observer));
+
     return () => {
       if (observer && deviceMockupRef.current) {
         observer.unobserve(deviceMockupRef.current);
+        window.removeEventListener('scroll', () => handleIntersection(observer.takeRecords(), observer));
       }
     };
   }, [deployedLink]);
