e283c1e91e38b12decf944dab112057f3359215d
Michael <fraynemichael300@gmail.com>
2023-05-23 14:06:21 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6919d02e2af9fa47eebe5a612f867900.js"
+  "/precache-manifest.15404f41eab1de98893a8eef4ac895d7.js"
 );
 
 workbox.clientsClaim();
