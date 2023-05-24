57b9ac2f82e82a44ba4bb6c6e79d65f57ba6c1fa
Michael <fraynemichael300@gmail.com>
2023-05-23 20:41:00 -0600

    made hero bigger

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -18,19 +18,19 @@ function Hero() {
   return (
     <section className="hero h-[100vh] grid grid-rows-12 mb-[10rem]">
       <div className='translate-y-[20vh] flex flex-col justify-center row-span-7'>
-        <h2 className='title h-[1em] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] leading-[5vw] text-[#89c4a7]'>Michael Frayne is...</h2>
+        <h2 className='title h-[1em] sm:text-[2rem] md:text-[3rem] lg:text-[4rem] leading-[28rem] text-[#89c4a7]'>Michael Frayne is...</h2>
         <div className='flex justify-center'>
           <img 
-            src="https://readme-typing-svg.herokuapp.com?font=Nunito&weight=800&size=96&duration=3000&pause=1000&color=B39DDB&center=true&vCenter=true&repeat=false&width=500&height=150&lines=MoonByte" 
+            src="https://readme-typing-svg.herokuapp.com?font=Nunito&weight=800&size=113&duration=3000&pause=1000&color=B39DDB&center=true&vCenter=true&repeat=false&width=555&height=150&lines=MoonByte" 
             alt="Typing SVG" 
             className='h-[4em] sm:h-[6em] md:h-[10em] lg:h-[13em]'
           />
         </div>
         <div className='flex justify-center'>
           <img 
-            src="https://readme-typing-svg.demolab.com?font=Nunito&weight=690&size=48&duration=1000&pause=2000&color=89C4A7&center=true&vCenter=true&width=300&lines=Developer,;Debugger,;+Designer," 
+            src="https://readme-typing-svg.herokuapp.com?font=Nunito&weight=700&size=48&duration=2000&pause=2000&color=89C4A7&center=true&vCenter=true&width=750&height=50&lines=Developer%2C+Debugger%2C+Designer" 
             alt="Typing SVG" 
-            className='h-[1em] sm:h-[2rem] md:h-[3rem] lg:h-[4rem]'
+            className='h-[1.1em] sm:h-[1.5rem] md:h-[2.2em] lg:h-[3.3em]'
           />
         </div>
       </div>
